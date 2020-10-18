d0e259e4cd97565606eca5de3988ec82072d769f
Silas Caimi <silascaimi@live.com>
2020-10-18 20:23:34 -0300

    Dependencia opcional com @Autowiowired

--- src/main/java/com/algawoks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algawoks/algafood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,40 @@
 package com.algawoks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algawoks.algafood.di.modelo.Cliente;
 import com.algawoks.algafood.di.notificacao.Notificador;
 import com.algawoks.algafood.di.notificacao.NotificadorEmail;
 
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired(required = false) //
 	private Notificador notificador;
 	
+	@Autowired
 	public AtivacaoClienteService(NotificadorEmail notificador) {
 		this.notificador = notificador;
 	}
 	
+//	public AtivacaoClienteService(String string) {
+//		
+//	}
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		if(notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		} else {
+			System.out.println("Cliente ativado sem notificador"); 
+		}
+		
 	}
 
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
+	}
+
+	
 }
